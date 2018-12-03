c06513564a7852fb6779a664cbc033ca1ca39375
Ali Dogan <alidogan.just.for.commits@gmail.com>
2018-12-03 11:59:50 +0100

    Initial Start Spring Boot commit

---
+++ src/main/java/nl/adogan/vitasimio/VitasimioApplication.java
@@ -0,0 +1,12 @@
+package nl.adogan.vitasimio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitasimioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitasimioApplication.class, args);
+	}
+}
